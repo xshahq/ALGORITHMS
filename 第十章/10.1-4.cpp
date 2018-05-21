8171a4a0cb31529b6b1936a5bde55c8ca3f72681
xshahq <15858265922@163.com>
2018-05-21 20:09:57 +0800

    algorithms

--- 第十章/10.1-4.cpp
+++ 第十章/10.1-4.cpp
@@ -6,7 +6,7 @@ struct QUEUE
 };
 int ENQUEUE(QUEUE& q,int x)
 {
-    if(q.head - 1 == q.tail)
+    if((!q.head && q.tail == N -1) || q.head - 1 == q.tail)
         return -1;
     arr[q.tail] = x;
     if(q.tail == N - 1)
@@ -19,8 +19,10 @@ int DEQUEUE(QUEUE& q)
 {
     if(q.head == q.tail)
         return -1;
+	int x = arr[q.head];
     if(q.head == N -1)
         q.head = 0;
-    q.head += 1;
-    return  q.head;
+	else
+    		q.head += 1;
+    return  x;
 }
---
+++ 第十章/10.1-5.cpp
@@ -0,0 +1,48 @@
+#define N 100
+int arr[N];
+struct QUEUE
+{
+    int tail,head;
+};
+int ENQUEUETAIL(QUEUE& q,int x)
+{
+    if((!q.head && q.tail == N -1) || q.head - 1 == q.tail)
+        return -1;
+    arr[q.tail] = x;
+    if(q.tail == N - 1)
+        q.tail = 0;
+    else
+        q.tail += 1;
+    return q.tail;
+}
+int ENQUEUEHEAD(QUEUE& q,int x)
+{
+    if((!q.head && q.tail == N -1) || q.head - 1 == q.tail)
+        return -1;
+    if(!q.head)
+        q.head = N -1;
+    else
+        q.head -= 1;
+    return q.head;
+}
+int DEQUEUEHEAD(QUEUE& q)
+{
+    if(q.head == q.tail)
+        return -1;
+    int x = arr[q.head];
+    if(q.head == N -1)
+        q.head = 0;
+    else
+        q.head += 1;
+    return  x;
+}
+int DEQUEUETAIL(QUEUE& q)
+{
+    if(q.head == q.tail)
+        return -1;
+    if(!q.tail)
+        q.tail = N - 1;
+    else
+        q.tail -= 1;
+    return arr[q.tail];
+}
