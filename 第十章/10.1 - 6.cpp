6f69cbee0bea37323ba72548c5021dede18708d9
xshahq <15858265922@163.com>
2018-05-22 21:14:43 +0800

    update

---
+++ 第十章/10.1 - 6.cpp
@@ -0,0 +1,26 @@
+struct QUEUE
+{
+    vector<int> v1,v2;//这里用vector来表示栈
+};
+void ENQUEUE(QUEUE& q,int x)
+{
+    while(q.v2.size())
+    {
+        q.v1.push_back(q.v2.back());
+        q.v2.pop_back();
+    }
+    q.v1.push_back(x);
+}
+int DEQUEUE(QUEUE& q)
+{
+    while(q.v1.size())
+    {
+        q.v2.push_back(q.v1.back());
+        q.v1.pop_back();
+    }
+    if(!q.v2.size())
+        return -1;
+    int temp = q.v2.back();
+    q.v2.pop_back();
+    return temp;
+}
