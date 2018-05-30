c36fb0af6b981c1d3c6ec74063c5b8f7e861f459
xshahq <15858265922@163.com>
2018-05-30 21:07:11 +0800

    2

--- 第十章/10.3-2.cpp
+++ 第十章/10.3-2.cpp
@@ -1,43 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define M 100
-int FREE = 0,arr[M][3];
-void initalize()
-{
-    for(int i = 0;i < M - 1;++i)
-    {
-        arr[i][2] = i + 1;
-        arr[i][1] = -1;
-    }
-    arr[M - 1][2] = -1;
-    arr[M - 1][1] = -1;
-}
-void allocate(int x)
-{
-    if(FREE == -1)
-        cout << "no space";
-    else
-    {
-        int temp = FREE;
-        arr[FREE][0] = x;
-        FREE = arr[FREE][2];
-        arr[FREE][1] = temp;
-    }
-}
-void FREEOBJECT(int x)
-{
-    if(arr[FREE][1] == -1)
-        cout << "it's empty";
-    else
-    {
-        if(arr[x][1] != -1)
-            arr[arr[x][1]][2] = arr[x][2];
-        if(arr[x][2] != -1)
-            arr[arr[x][2]][1] = arr[x][1];
-        if(arr[FREE][1] != -1)
-            arr[arr[FREE][1]][2] = x;
-        arr[x][1] = arr[FREE][1];
-        arr[x][2] = FREE;
-        FREE = x;
-    }
-}
+#define Null -1  
+int head=0,Free=0;
+int Allocate_object(int *Array,int*num)  
+{  
+    static x;  
+    if (Free==Null)  
+    {    
+        cout<<"out of space!"<<endl;  
+        return Null;  
+    }  
+    else  
+    {     
+        x=Free;  
+        Free=Array[x];  
+        return x;  
+    }  
+}  
+void Free_object(int *Array,int x)  
+{  
+    Array[x]=Free;  
+    Free=x;  
+}  
