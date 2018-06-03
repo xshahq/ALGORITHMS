82ea3f59f7e91045055a070c59dbc94ae7aebb15
xshahq <15858265922@163.com>
2018-06-03 20:12:20 +0800

    3

--- 第六章/6.2-5.cpp
+++ 第六章/6.2-5.cpp
@@ -1,14 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
-void MIN_HEAP(int* arr,int i,int length)
+void MAX_HEAP(int* arr,int i,int length)
 {
     while(1)
     {
         int l = 2 * i + 1,r = 2 * i + 2,temp = i;
         if(l < length)
-            temp = arr[temp] < arr[l] ? temp : l;
+            temp = arr[temp] > arr[l] ? temp : l;
         if(r < length)
-            temp = arr[temp] < arr[r] ? temp : r;
+            temp = arr[temp] > arr[r] ? temp : r;
         if(temp == i)
             break;
         swap(arr[temp],arr[i]);
