e45efeea4f2a218dc1b0edf158d830a1d36f612c
xshahq <15858265922@163.com>
2018-06-07 21:46:22 +0800

    2

---
+++ 第六章/6.5-7.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+void DELECT(int* arr,int j,int& length)
+{
+    if(j >= length)
+        cout << "overflow";
+    else
+    {
+        arr[j] = INT_MIN;
+        int i = j;
+        while(1)
+        {
+            int l = 2 * i + 1,r = 2 * i + 2,temp = i;
+            if(l < length)
+                temp = arr[temp] > arr[l] ? temp : l;
+            if(r < length)
+                temp = arr[temp] > arr[r] ? temp : r;
+            if(temp == i)
+                break;
+            swap(arr[temp],arr[i]);
+            i = temp;
+        }
+        --length;
+        if(j < length)
+            swap(arr[j],arr[j + 1]);
+    }
+}
